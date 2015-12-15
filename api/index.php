61c5126afe4eff2869d1b24c33e6f702b82989dc
Konstantin Sidorenko <konstantin@yellowcake.net>
2015-12-15 15:07:05 +0100

    fix des post et put

--- api/index.php
+++ api/index.php
@@ -3,16 +3,16 @@
 require 'vendor/autoload.php';
 
 //connexion a la db
-use Illuminate\Database\Capsule\Manager as Capsule;  
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-$capsule = new Capsule; 
+$capsule = new Capsule;
 
 $capsule->addConnection(array(
     'driver'    => 'mysql',
     'host'      => 'localhost',
     'database'  => 'Foyer',
     'username'  => 'root',
-    'password'  => 's3curit3',
+    'password'  => 'korsi29yk',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => ''
--- api/routes/product.php
+++ api/routes/product.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
-use Illuminate\Database\Capsule\Manager as Capsule;  
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 $app->group('/product', function() use ($app) {
 
 	//get all
 	$app->get('/', function($request, $response) {
-		try {	
+		try {
 			$response = $response->withJson(Capsule::table('PRODUCT')->get());
 		} catch(Illuminate\Database\QueryException $e) {
 			$response = $response->withJson(array ("status"  => array("error" => $e->getMessage())), 400);
@@ -16,7 +16,7 @@ $app->group('/product', function() use ($app) {
 
 	//get by id_product
 	$app->get('/id_product/{id_product}', function($request, $response, $id_product){
-		try {	
+		try {
 			$response = $response->withJson(Capsule::table('PRODUCT')->where('id_product', $id_product)->first());
 		} catch(Illuminate\Database\QueryException $e) {
 			$response = $response->withJson(array ("status"  => array("error" => $e->getMessage())), 400);
@@ -27,7 +27,7 @@ $app->group('/product', function() use ($app) {
 	//add product
 	$app->post('/',function ($request, $response)  use ($app) {
 		try {
-			Capsule::table('PRODUCT')->insert(json_decode($app->request()));
+			Capsule::table('PRODUCT')->insert($request->getParsedBody());
 			$response = $response->withJson(array ("status"  => array("success" => "ok")), 200);
 		} catch(Illuminate\Database\QueryException $e) {
 			$response = $response->withJson(array ("status"  => array("error" => $e->getMessage())), 400);
@@ -38,8 +38,7 @@ $app->group('/product', function() use ($app) {
 	//edit product
 	$app->put('/{id_product}', function ($request, $response, $id_product) use ($app){
 		try {
-			var_dump($app->request());
-			Capsule::table('PRODUCT')->where('id_product',$id_product)->update(json_decode($app->request()));
+			Capsule::table('PRODUCT')->where('id_product',$id_product)->update($request->getParsedBody());
 			$response = $response->withJson(array ("status"  => array("success" => "ok")), 200);
 		} catch(Illuminate\Database\QueryException $e) {
 			$response = $response->withJson(array ("status"  => array("error" => $e->getMessage())), 400);
