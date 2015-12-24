97b364c5ce4055ceeb2a62e03abbbb6854b8bb6f
Konstantin <konstantin.sidorenko@orange.fr>
2015-12-24 15:32:53 +0100

    ajout des notifs + log api

--- api/config/config.php
+++ api/config/config.php
@@ -11,4 +11,11 @@ define('DB_COLLATION','utf8_unicode_ci');
 define('DB_PREFIX','');
 //dossier des fichiers
 define('DIR_FILES','./files/');
-
+//log de l'api
+define('API_USER','root');
+define('API_PASSWORD','s3curit3');
+//message de notifs
+define('NOTIF_COMMAND_STATE_0','Delete');
+define('NOTIF_COMMAND_STATE_1','En cours de validation');
+define('NOTIF_COMMAND_STATE_2','Validée');
+define('NOTIF_COMMAND_STATE_3','Payée');
