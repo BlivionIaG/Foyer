eb4fe159017e3e0e26308c975e8fda93fc51f688
Konstantin <konstantin.sidorenko@orange.fr>
2016-02-29 15:09:11 +0100

    Update directives.js

--- app_admin/js/directives/directives.js
+++ app_admin/js/directives/directives.js
@@ -2,23 +2,21 @@
 
 angular.module('foyerApp.directives', [])
 //ajout de la directive ng-confirm-click
-.directive('ngConfirmClick', [
-  function(){
-    return {
-      priority: -1,
-      restrict: 'A',
-      link: function(scope, element, attrs){
-        element.bind('click', function(e){
-          var message = attrs.ngConfirmClick;
-          if(message && !window.confirm(message)){
-            e.stopImmediatePropagation();
-            e.preventDefault();
-          }
-        });
-      }
-    };
-  }
-  ])
+.directive('ngConfirmClick', [ function(){
+  return {
+    priority: -1,
+    restrict: 'A',
+    link: function(scope, element, attrs){
+      element.bind('click', function(e){
+        var message = attrs.ngConfirmClick;
+        if(message && !window.confirm(message)){
+          e.stopImmediatePropagation();
+          e.preventDefault();
+        }
+      });
+    }
+  };
+}])
 
 .directive('fileModel', ['$parse', function ($parse) {
   return {
@@ -26,7 +24,6 @@ angular.module('foyerApp.directives', [])
     link: function(scope, element, attrs) {
       var model = $parse(attrs.fileModel);
       var modelSetter = model.assign;
-
       element.bind('change', function(){
         scope.$apply(function(){
           modelSetter(scope, element[0].files[0]);
