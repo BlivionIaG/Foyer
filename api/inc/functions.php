df620043492570232ac7b2f795bf720c53d5a734
Konstantin <konstantin.sidorenko@orange.fr>
2016-03-04 14:24:22 +0100

    fix auth

--- api/inc/functions.php
+++ api/inc/functions.php
@@ -1,11 +1,12 @@
 <?php
 
 //fonction pour check la connexion à l'api
-function checkAuth($user, $key){
+function checkAuth($user, $password){
   $API_USER = json_decode(API_USER);
-  foreach ($API_USER as $key => $login)
-    if($login->user == $user && $login->password == $key)
-      return $login;
-
+  foreach ($API_USER as $login){
+    if($login->user == $user && $login->password == $password){
+       return $login;
+    }
+  }
   return false;
 }
