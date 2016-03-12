4928e6045306ca049fa4e8341fdaa9fabeb6abfe
renald-morice <renaldmorice@hotmail.fr>
2016-03-12 22:42:58 +0100

    Première interface de connexion à l'ISEN

--- app_mobile/iphone/FoyerISEN/FoyerISEN/ViewController.swift
+++ app_mobile/iphone/FoyerISEN/FoyerISEN/ViewController.swift
@@ -11,16 +11,24 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate,  NetworkManagerDelegate {
     
+    //ALERT
+    var alertController = UIAlertController(title: "", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+    
+    //Network
     var networkManager = NetworkManager.sharedInstance
 
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var passwdTextField: UITextField!
-    @IBOutlet weak var label: UILabel!
+    //@IBOutlet weak var label: UILabel!
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     @IBOutlet weak var connectionButton: UIButton!
+    @IBOutlet weak var fieldView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Ajout de l'action (bouton OK) à l'alert
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel,handler: nil))
         
         //Looks for single or multiple taps.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
@@ -30,6 +38,8 @@ class ViewController: UIViewController, UITextFieldDelegate,  NetworkManagerDele
         passwdTextField.delegate = self
         indicator.hidden = true
         
+        fieldView.layer.cornerRadius = 5
+        connectionButton.layer.cornerRadius = 2
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,7 +49,7 @@ class ViewController: UIViewController, UITextFieldDelegate,  NetworkManagerDele
     
     // Bouton de connexion appuyé :
     //-----------------------------
-    @IBAction func connexionTouched(sender: AnyObject) {
+    @IBAction func connectionButtonTouched(sender: AnyObject) {
         
         connectionButton.hidden = true
         indicator.hidden = false
@@ -51,7 +61,6 @@ class ViewController: UIViewController, UITextFieldDelegate,  NetworkManagerDele
         ]
         networkManager.request(delegate : self, urlString: "http://foyer.p4ul.tk/api/cas/", requestType: "POST", postParams: postParams)
     }
-    
     
     //Calls this function when the tap is recognized.
     func dismissKeyboard() {
@@ -78,22 +87,29 @@ class ViewController: UIViewController, UITextFieldDelegate,  NetworkManagerDele
                 if let tabStatus : NSDictionary = data["status"] as? NSDictionary {
                     
                     if let errorString : String = tabStatus["error"] as? String {
-                        label.text = errorString
+
+                        alertController.title = "errorString  : "
+                        alertController.message = errorString
                         
                     } else {
                         
                         if ( tabStatus["key"] == nil || tabStatus["username"] == nil){
-                            label.text = "Problème de connexion : contacter le support !"
-                            print("Pas d'erreur d'ID mais soit l'object JSON Key ou Username est nul !")
+                            
+                            alertController.title = ""
+                            alertController.message = "Pas d'erreur d'ID mais soit l'object JSON Key ou Username est nul !"
+                            
                         } else{
                             networkManager.authBasicKey = tabStatus["key"] as? String
                             networkManager.username = tabStatus["username"] as? String
-                            label.text = "Connexion établie"
+                            
+                            alertController.title = "Connexion établie"
+                            alertController.message = ""
                         }
                     }
                     
                 }
             }
+            
             
         }
         //Une fois que la réponse est finie d'être traitée
@@ -101,13 +117,19 @@ class ViewController: UIViewController, UITextFieldDelegate,  NetworkManagerDele
         indicator.hidden = true
         connectionButton.hidden = false
         
+        //Affichage de l'alerte
+        presentViewController(alertController, animated: true, completion: nil)
+        
     }
     
     // Probème de connexion :
     //-----------------------
     func didFailToReceiveResponse(strError : String) {
-        label.text = "Problème de connexion : contacter le support !"
-        print( "UIViewController : \(strError)")
+        
+        alertController.title = "UIViewController : "
+        alertController.message = "\(strError)"
+        //Affichage de l'alerte
+        presentViewController(alertController, animated: true, completion: nil)
     }
     
     //-------------------------
