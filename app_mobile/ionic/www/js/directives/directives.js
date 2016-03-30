9723f8ec3f51de2abbc9760996fbd29c131f682c
Konstantin Sidorenko <konstantin@yellowcake.net>
2016-03-30 16:02:53 +0200

    fix codacy

--- app_mobile/ionic/www/js/directives/directives.js
+++ app_mobile/ionic/www/js/directives/directives.js
@@ -5,12 +5,12 @@ angular.module('starter.directives', [])
 .directive('onFinishRender', function ($timeout) {
 	return {
 		restrict: 'A',
-		link: function (scope, element) {
+		link: function (scope) {
 			if (scope.$last === true) {
 				$timeout(function () {
 					scope.$emit('ngRepeatFinished');
 				});
 			}
 		}
-	}
+	};
 });
