d6fa00c75cf29e4e819892415cbbbe71517487ed
Konstantin Sidorenko <konstantin@yellowcake.net>
2016-03-30 10:17:19 +0200

    fix bugs

--- app_mobile/ionic/www/js/app.js
+++ app_mobile/ionic/www/js/app.js
@@ -7,7 +7,7 @@ angular.module('starter',[
   'starter.services',
   'starter.directives',
   'starter.filters'
-])
+  ])
 
 .constant('CONFIG', {
   API_URL: 'http://isenclub.fr/foyer/api/'
@@ -106,31 +106,27 @@ angular.module('starter',[
       var name, value, fullSubName, subName, subValue, innerObj, i;
       
       for(name in obj){
-        if ({}.hasOwnProperty.call(name, obj)){
-          value = obj[name];
-          
-          if(value instanceof Array){
-            for(i=0; i<value.length; ++i){
-              subValue = value[i];
-              fullSubName = name + '[' + i + ']';
-              innerObj = {};
-              innerObj[fullSubName] = subValue;
-              query += param(innerObj) + '&';
-            }
-          }else if(value instanceof Object){
-            for(subName in value){
-              if ({}.hasOwnProperty.call(subName, value)){
-                subValue = value[subName];
-                fullSubName = name + '[' + subName + ']';
-                innerObj = {};
-                innerObj[fullSubName] = subValue;
-                query += param(innerObj) + '&';
-              }
-            }
+        value = obj[name];
+        
+        if(value instanceof Array){
+          for(i=0; i<value.length; ++i){
+            subValue = value[i];
+            fullSubName = name + '[' + i + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
           }
-          else if(value !== undefined && value !== null){
-            query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+        }else if(value instanceof Object){
+          for(subName in value){
+            subValue = value[subName];
+            fullSubName = name + '[' + subName + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
           }
+        }
+        else if(value !== undefined && value !== null){
+          query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
         }
       }
       return query.length ? query.substr(0, query.length - 1) : query;
